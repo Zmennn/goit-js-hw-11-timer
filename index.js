2c858ce5a30290f7fa8bc87821fb834a169e8304
Zmennn <79196210+Zmennn@users.noreply.github.com>
2021-08-08 21:22:01 +0300

    0.8

--- index.js
+++ index.js
@@ -1,33 +1,83 @@
-const targetDate = new Date('Aug 9, 2021');
-const refs = {
-    days: document.querySelector('[data-value="days"]'),
-    hours: document.querySelector('[data-value="hours"]'),
-    mins: document.querySelector('[data-value="mins"]'),
-    secs: document.querySelector('[data-value="secs"]'),
-}
-const timerId = setInterval(timeToDate, 1000, targetDate);
+// const targetDate = new Date('Aug 8, 2021');
+// const refs = {
+//     days: document.querySelector('[data-value="days"]'),
+//     hours: document.querySelector('[data-value="hours"]'),
+//     mins: document.querySelector('[data-value="mins"]'),
+//     secs: document.querySelector('[data-value="secs"]'),
+// }
+// const timerId = setInterval(timeToDate, 1000, targetDate);
 
-function pad(number) {
-    return String(number).padStart(2, "0")
-};
+// function pad(number) {
+//     return String(number).padStart(2, "0")
+// };
 
 
-function timeToDate(targetDate) {
+// function timeToDate(targetDate) {
 
-    const currentDate = Date.now();
-    const timeLeft = targetDate.getTime() - currentDate;
-    if (timeLeft < 0) {
-        clearInterval(timerId);
-        return
+//     const currentDate = Date.now();
+//     const timeLeft = targetDate.getTime() - currentDate;
+//     if (timeLeft < 0) {
+
+//         clearInterval(timerId);
+//         return
+//     };
+
+//     const days = pad(Math.floor(timeLeft / (1000 * 60 * 60 * 24)));
+//     const hours = pad(Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+//     const mins = pad(Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)));
+//     const secs = pad(Math.floor((timeLeft % (1000 * 60)) / 1000));
+
+//     refs.days.textContent = days;
+//     refs.hours.textContent = hours;
+//     refs.mins.textContent = mins;
+//     refs.secs.textContent = secs;
+// }
+
+
+
+class CountdownTimer {
+    constructor({ selector, targetDate }) {
+        this.targetDate = targetDate;
+        this.refs = {
+            days: document.querySelector(`${selector} [data-value="days"]`),
+            hours: document.querySelector(`${selector} [data-value="hours]`),
+            mins: document.querySelector(`${selector} [data-value="mins]`),
+            secs: document.querySelector(`${selector} [data-value="secs"]`),
+        };
+        this.timerId = setInterval(this.timeToDate, 1000, this.targetDate);
+    }
+
+
+
+    timeToDate(targetDate) {
+
+        const currentDate = Date.now();
+        const timeLeft = targetDate.getTime() - currentDate;
+        if (timeLeft < 0) {
+
+            clearInterval(this.timerId);
+            return
+        };
+
+        const days = String(Math.floor(timeLeft / (1000 * 60 * 60 * 24))).padStart(2, "0");
+        const hours = this.pad(Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        const mins = this.pad(Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)));
+        const secs = this.pad(Math.floor((timeLeft % (1000 * 60)) / 1000));
+
+        refs.days.textContent = days;
+        refs.hours.textContent = hours;
+        refs.mins.textContent = mins;
+        refs.secs.textContent = secs;
     };
 
-    const days = pad(Math.floor(timeLeft / (1000 * 60 * 60 * 24)));
-    const hours = pad(Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-    const mins = pad(Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)));
-    const secs = pad(Math.floor((timeLeft % (1000 * 60)) / 1000));
-
-    refs.days.textContent = days;
-    refs.hours.textContent = hours;
-    refs.mins.textContent = mins;
-    refs.secs.textContent = secs;
+    pad(number) {
+        return String(number).padStart(2, "0")
+    };
 }
+
+
+
+new CountdownTimer({
+    selector: '#timer-1',
+    targetDate: new Date('Aug 10, 2021'),
+});
